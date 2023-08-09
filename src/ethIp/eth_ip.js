2dbe73c62153207074901446f41dc89449b40d36
Jovani Sanchez <jovani.controler@outlook.com>
2023-08-09 08:54:34 -0600

    Multiples Tags

--- src/ethIp/eth_ip.js
+++ src/ethIp/eth_ip.js
@@ -1,9 +1,17 @@
-import { Tag, Controller } from "ethernet-ip";
+import { Tag, Controller, TagGroup } from "ethernet-ip";
 import { insertData } from "./controlador/tags_ctrl.js";
 
 const PLC = new Controller();
+const PLC_Tags = new TagGroup();
+
+PLC_Tags.add(new Tag("BOTON", "MainProgram"));
+PLC_Tags.add(new Tag("F_1", "MainProgram"));
+//PLC.subscribe() */
+
+//PLC.subscribe(PLC_Tags);
 
 PLC.subscribe(new Tag("BOTON", "MainProgram"));
+PLC.subscribe(new Tag("F_1", "MainProgram"));
 
 export function selladora() {
   PLC.connect("192.168.100.101", 0).then(async () => {
@@ -12,18 +20,28 @@ export function selladora() {
   });
 }
 
-PLC.forEach(tag => {
+async function RG_Tags() {
+  await PLC.readTagGroup(PLC_Tags);
+  PLC_Tags.forEach((tag) => {
+   var txt = {
+      name: tag.name,
+      value: tag.value
+    }
+    console.table(txt);
+  });
+}
 
-  tag.on("Initialized", tag => {
-      console.log("Initialized", tag.value);
-      insertData(tag.value.toString());
+PLC.forEach((tag) => {
+  tag.on("Initialized", (tag) => {
+    console.log("Initialized", tag.name, " = ",tag.value);
+    //insertData(tag.value.toString());
   });
 
   tag.on("Changed", (tag, oldValue) => {
-      
-      console.log(tag.name)
-      console.log("Changed:", tag.value);
-      insertData(tag.value.toString());
-      
+    console.log(tag.name, "Changed to:", tag.value);
+    RG_Tags();
+    //insertData(tag.value.toString());
   });
 });
+
+selladora();
--- src/ethIp/eth_ip_ex.js
+++ src/ethIp/eth_ip_ex.js
@@ -1,39 +1,49 @@
-import nodepccc from 'nodepccc';
-var conn = new nodepccc;
+import nodepccc from "nodepccc";
+var conn = new nodepccc();
 var doneReading = false;
 var doneWriting = false;
 var preVal = false;
 
 const intervalID = setInterval(connected, 500);
 
-conn.initiateConnection({port: 44818, host: '192.168.100.27'}, intervalID);
+conn.initiateConnection({ port: 44818, host: "192.168.100.27" }, intervalID);
 
 function connected(err) {
-	if (typeof(err) !== "undefined") {
-		console.log(err);
-		process.exit();
-	}
-	conn.setTranslationCB(tagLookup);
-	conn.addItems('VarBool');
-	conn.readAllItems(valuesReady);	
+  if (typeof err !== "undefined") {
+    console.log(err);
+    process.exit();
+  }
+  conn.setTranslationCB(tagLookup);
+  conn.addItems("VarBool");
+  conn.readAllItems(valuesReady);
 }
 
 function valuesReady(anythingBad, values) {
-	if (anythingBad) { console.log("SOMETHING WENT WRONG READING VALUES!!!!"); }
-    if (preVal != values.VarBool)  {
+  if (anythingBad) {
+    console.log("SOMETHING WENT WRONG READING VALUES!!!!");
+  }
+  /*  if (preVal != values.VarBool)  {
         preVal = values.VarBool 
         console.log(values.VarBool)
-    } 
-
-	doneReading = true;
-	if (doneWriting) { process.exit(); }
+    }  */
+  console.log(values);
+  doneReading = true;
+  if (doneWriting) {
+    process.exit();
+  }
 }
 
 function tagLookup(tag) {
-	switch (tag) {
-	case 'VarBool':
-		return 'B3:0/0';				//Bool
-	default:
-		return undefined;
-	}
+  switch (tag) {
+    case "Var1":
+      return "B16:0/0"; //Bool
+    case "Var2":
+      return "B16:0/8";
+    case "Var3":
+      return "B16:0/2";
+    case "Var4":
+      return "N10:10";
+    default:
+      return undefined;
+  }
 }
